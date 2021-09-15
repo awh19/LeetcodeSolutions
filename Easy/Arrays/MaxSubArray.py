bd3e4c11e899b13a864184140248a474042e172d
Andrew Hill <ahill@nethealth.com>
2021-09-15 14:34:04 -0400

    Max Sub Array Alternate Solution

--- Easy/Arrays/MaxSubArray.py
+++ Easy/Arrays/MaxSubArray.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def maxSubArray(self, nums: List[int]) -> int:
         
         maxSum = nums[0]
@@ -15,3 +15,13 @@ class Solution:
 # Notes Learned:
 # - Make sure to realize that the max sum doesn't need to include the negative number at beginning of sub array
 # - If subArray goes to 0 it is not a good subarray.
+
+
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currMax=globalMax=nums[0]
+        for i in range(1,len(nums)):
+            currMax = max(nums[i],currMax+nums[i])
+            if currMax > globalMax:
+                globalMax = currMax
+        return globalMax
